768a7f20d6832de6c8e79a4a62df655ae73bda32
DISCLAIMER-RYTP <andriy379mat@gmail.com>
2021-08-29 20:47:01 +0300

    Connect MainPage and add Error404 Page
    Add links:
    *cs-go
    *valorant
    *error404

--- AKUMA/src/app/app-routing.module.ts
+++ AKUMA/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from 'src/Pages&Components/MainPage/MainPage.component';
+import { CSGOPageComponent } from 'src/Pages&Components/CSGOPage/CSGOPage.component';
+import { ValorantPageComponent } from 'src/Pages&Components/ValorantPage/ValorantPage.component';
+import { Error404Component } from 'src/Pages&Components/Error404/Error404.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: MainPageComponent},
+  { path: 'cs-go', component: CSGOPageComponent},
+  {path: 'valorant', component: ValorantPageComponent},
+  {path: '**', component: Error404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
